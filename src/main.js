313ec532cbcfd90b78ffe8cbcdc990a4006df0b3
Fabio Tea <iam@f4b.io>
2023-12-28 05:03:37 +0100

    save

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import "./style.css";
+import * as VM from "@violentmonkey/dom";
 
 VM.observe(document.body, () => {
-  console.log("better-transmission-tabscontainer is running");
-
   // Find the target node
   const tabsContainer = document.querySelector(".tabs-container");
   // const tabsContainer = document.querySelector('#inspector');
 
   if (tabsContainer) {
-    tabsContainer.style.resize = "both";
+    console.log("found better-transmission-tabscontainer's tabsContainer");
+
+    tabsContainer.style.resize = "horizontal";
     tabsContainer.style.overflow = "auto";
-    tabsContainer.style.width = "700px";
+    tabsContainer.style.width = "900px";
 
     // disconnect observer
     return true;
--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,18 @@ export default defineConfig({
     monkey({
       entry: "src/main.js",
       userscript: {
+        name: "Better Transmission TabsContainer",
+        author: "Fabio Tea <iam@f4b.io>",
+        locale: ["en"],
+        license: "MIT",
+        description:
+          "A userscript to make Transmission Web's TabsContainer better",
         icon: "https://vitejs.dev/logo.svg",
         namespace: "npm/vite-plugin-monkey",
-        match: [
-          "*://*/*"
-        ],
+        match: ["*://*/transmission/web/"],
         grant: ["none"],
-        require: [
-          "https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2"
-        ]
+        require: ["https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2"],
+        server: { mountGmApi: true },
       },
     }),
   ],
